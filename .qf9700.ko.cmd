4752b86dc1d9a7f498b582ff73c970313d1fd7d6
Pau Garcia i Quiles <pgquiles@elpauer.org>
2015-11-09 14:20:32 +0100

    'make clean' removed generated files

--- .qf9700.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/liujl/liujl/usb2net/sr9700/cp9700_driver/qf9700.ko := ld -r -m elf_i386  --build-id -o /home/liujl/liujl/usb2net/sr9700/cp9700_driver/qf9700.ko /home/liujl/liujl/usb2net/sr9700/cp9700_driver/qf9700.o /home/liujl/liujl/usb2net/sr9700/cp9700_driver/qf9700.mod.o
--- .tmp_versions/qf9700.mod
+++
@@ -1,2 +0,0 @@
-/home/liujl/liujl/usb2net/sr9700/cp9700_driver/qf9700.ko
-/home/liujl/liujl/usb2net/sr9700/cp9700_driver/qf9700.o
